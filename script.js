6e71ea0b121b9fd5637fd1ba8254e74ddc28506d
Jack Arthur <Jack@Jacks-MacBook-Air-2.local>
2017-11-08 20:25:23 +0000

    added hover effect

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ $( document ).ready(function(){
   	'Coming Soon',
   	function(n){ return n.toString(); });
   var remainder = countdown(new Date(2017, 10, 15),function(ts){$("#countdown").text(ts.toString());});
+  $('#pupil').hover(function() {
+      $(this).fadeTo("slow",1);
+  },function() {
+      $(this).fadeTo("slow",0);
+  });
 });
