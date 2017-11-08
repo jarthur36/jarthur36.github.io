772f99cd6ec7f893a509fe44ea738e744bec9767
Jack Arthur <Jack@Jacks-MacBook-Air-2.local>
2017-11-08 21:40:19 +0000

    final upgrade

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ $( document ).ready(function(){
   	' | | | | | | | | | |',
   	': ',
   	': ',
-  	'Coming Soon',
+  	'Prepare for Upload',
   	function(n){ return n.toString(); });
   var remainder = countdown(new Date(2017, 10, 15),function(ts){
     $("#countdown").text(ts.toString());
-    if(ts.seconds%4==0){
-      $("#pupil").fadeTo(2000,1);
-    }else if(ts.seconds%4==2){
-      $("#pupil").fadeTo(2000,0);
-    }
+    //if(ts.seconds%4==0){
+    //  $("#pupil").fadeTo(2000,1);
+    //}else if(ts.seconds%4==2){
+    //  $("#pupil").fadeTo(2000,0);
+    //}
 });
   $('#pupil').hover(function() {
       $(this).fadeTo("slow",1);
