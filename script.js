4ad6764ce4c7456857dcb193da0b5d81d918a7fb
Jack Arthur <Jack@Jacks-MacBook-Air-2.local>
2017-11-08 20:55:35 +0000

    adding pulsing

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ $( document ).ready(function(){
   	': ',
   	'Coming Soon',
   	function(n){ return n.toString(); });
-  var remainder = countdown(new Date(2017, 10, 15),function(ts){$("#countdown").text(ts.toString());});
+  var remainder = countdown(new Date(2017, 10, 15),function(ts){
+    $("#countdown").text(ts.toString());
+    if(ts.seconds%4==0){
+      $("#pupil").fadeTo(2000,1);
+    }else if(ts.seconds%4==2){
+      $("#pupil").fadeTo(2000,0);
+    }
+});
   $('#pupil').hover(function() {
       $(this).fadeTo("slow",1);
   },function() {
